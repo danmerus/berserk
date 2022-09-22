224b96326472e18058d330a3c365df8a7957b7f2
danmerus <leoghost@mail.ru>
2022-09-22 11:11:34 +0500

    server ready?

--- game.py
+++ game.py
@@ -586,6 +586,7 @@ class Game:
             # print('send_state:', player, self.curr_priority)
             self.gui.on_state_received(state)
         self.send_complete = True
+        time.sleep(0.1)
 
     def on_reveal(self, cards):
         for card in cards:
